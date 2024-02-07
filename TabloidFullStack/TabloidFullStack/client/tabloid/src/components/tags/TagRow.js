2ae1f672a0c67638bd5cbe61b23730bdfc133af3
Logan Van Meter <vanmeterlj@gmail.com>
2024-02-07 10:54:46 -0500

    Delete tags

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/tags/TagRow.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/tags/TagRow.js
@@ -1,12 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import { Button } from "reactstrap";
 
 export const TagRow = ({ tag }) => {
+	const navigate = useNavigate();
+	const handleNavigate = (e) => {
+		e.preventDefault();
+		const [, tagId] = e.target.id.split("--");
+		navigate(`/Tags/Delete/${tagId}`);
+	};
+
 	return (
 		<tr>
 			<th scope='row'>{tag.id}</th>
 			<td>{tag.name}</td>
 			<td>
-				<Button id={`delete-tag--${tag.id}`} color='danger'>
+				<Button
+					id={`delete-tag--${tag.id}`}
+					color='danger'
+					onClick={(e) => handleNavigate(e)}
+				>
 					Delete
 				</Button>
 			</td>
