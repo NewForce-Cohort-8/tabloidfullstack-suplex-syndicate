c4ff30d21b6242c3d7fa496181c9392c80808f4d
MaxAguilar712 <a26001977@gmail.com>
2024-02-08 15:49:10 -0500

    add cats working

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -3,9 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
 import { TagList } from "./tags/TagList.js";
 import { AddTag } from "./tags/AddTag.js";
+import { AddCategory } from "./categories/CategoryForm.js";
 import { CategoryList } from "./categories/CategoryList.js";
 
 import PostList from "./PostList";
+// import AddCategory from "./categories/CategoryForm.js";
 
 export default function ApplicationViews() {
 	return (
@@ -14,6 +16,7 @@ export default function ApplicationViews() {
 			<Route path='/Tags' element={<TagList />} />
 			<Route path='/Tags/Add' element={<AddTag />} />
       <Route path="/Categories" element={<CategoryList />} />
+      <Route path="/categories/form" element={<AddCategory />} />
         <Route path="/posts" element={<PostList />} />
 
 		</Routes>
